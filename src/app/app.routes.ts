914d8ba8334ff09c17a351d2954e6759b88d6f4b
ushiovii <ushiovii@hotmail.com>
2026-07-14 14:50:49 -0300

    fix(front): sacar links muertos de navbar y lazy-load del globo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import { Globe } from './globe/globe';
 
 export const routes: Routes = [
-  { path: 'globe', component: Globe },
+  { path: 'globe', loadComponent: () => import('./globe/globe').then((m) => m.Globe) },
   { path: 'login', loadComponent: () => import('./pages/login').then((m) => m.Login) },
   { path: 'register', loadComponent: () => import('./pages/register').then((m) => m.Register) },
   { path: '', pathMatch: 'full', loadComponent: () => import('./pages/home').then((m) => m.Home) },
